7c8b25fa77887c29b4120b424567917a403fd8c2
Shrawak Shakya <shrawak@gmail.com>
2026-03-06 03:54:13 -0800

    cleaned up

--- agents/run_vid_agent.ps1
+++
@@ -1,6 +0,0 @@
-$env:SUPABASE_URL = "https://etmcvnfgueyoauwlmaql.supabase.co"
-$env:SUPABASE_BUCKET = "detectBucket"
-$env:SUPABASE_PATH_PREFIX = "edited"
-$env:SUPABASE_SERVICE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0bWN2bmZndWV5b2F1d2xtYXFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTM5MTcyNCwiZXhwIjoyMDc2OTY3NzI0fQ.M0Ol0GfRF3_IkZT2upUJpnHoV3qr2ImS7ayoEFgFPqI"
-
-python -m src.agents.video_editor_agent
